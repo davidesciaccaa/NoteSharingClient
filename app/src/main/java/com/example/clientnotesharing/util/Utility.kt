d02f577f89597390cca0f5ff5c8bd2325dc658b4
Ylli <ybraci@studenti.uninsubria.it>
2024-07-02 11:47:46 +0200

    Sostituzione metodi deprecati da getGeoPointFromAddress in Utility

--- app/src/main/java/com/example/clientnotesharing/util/Utility.kt
+++ app/src/main/java/com/example/clientnotesharing/util/Utility.kt
@@ -1,15 +1,20 @@
 package com.example.clientnotesharing.util
 
 import android.content.Context
+import android.location.Address
 import android.location.Geocoder
 import android.location.Geocoder.GeocodeListener
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.tomtom.sdk.location.GeoPoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.util.Locale
+import java.util.concurrent.Semaphore
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class Utility {
 
@@ -23,18 +28,25 @@ class Utility {
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     suspend fun getGeoPointFromAddress(context: Context, address: String): GeoPoint? {
         return withContext(Dispatchers.IO) {
-            val geocoder = Geocoder(context, Locale.ITALY)
+            suspendCoroutine<GeoPoint?> { continuation ->
+                val geocoder = Geocoder(context, Locale.ITALY)
+                geocoder.getFromLocationName(address, 1, object : Geocoder.GeocodeListener {
+                    override fun onGeocode(addresses: MutableList<Address>) {
+                        val geopoint = if (addresses.isNotEmpty()) {
+                            val address = addresses[0]
+                            val latitude = address.latitude
+                            val longitude = address.longitude
+                            GeoPoint(latitude, longitude)
+                        } else {
+                            null
+                        }
+                        continuation.resume(geopoint)
+                    }
 
-            //************
-            val addresses = geocoder.getFromLocationName(address, 1)
-
-            if (addresses?.isNotEmpty() == true) {
-                val address = addresses.get(0)
-                val latitude = address.latitude
-                val longitude = address.longitude
-                GeoPoint(latitude, longitude)
-            } else {
-                null
+                    override fun onError(errorMessage: String?) {
+                        continuation.resume(null) // Handle error case
+                    }
+                })
             }
         }
     }
